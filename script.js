771c1b7e7b4d620812c3af932597c2ccf36f10a9
Miglia-Schicker <schickmi50@craighead.school.nz>
2024-08-20 10:22:50 +1200

    testing
    tested guess the number answers
    tested all numbers up to 20
    answers were : 11, 19, 4

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
-let numbers = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20
-]
 
-let result = [
+// array to store scores
+let results = [
 
 ]
 
+// asks the user for their name and stored and uses the name variable to introduce the game
 function GuessTheNumber() {
     let name = prompt('What is your name?')
     alert ('Hi ' + name + '! I am thinking of a number between 1 and 20 inclusive. Try and guess the number in the least amount of tries.')
 
-    let index = Math.floor (Math.random()*20)
-    let number = answers[index]
+// creating variables and giving them a initial value before they get changed further into the code
+    let userScore = 0
+    let userGuess = 0
+    let answer = Math.floor (Math.random()*19)
+    answer = answer + 1
+    while (userGuess != answer){
+        userGuess = prompt ('Guess what the number is!')
+        alert ('That was wrong. Try again')
+        userScore = userScore + 1
+ } if (userGuess == answer){
+    alert('Well done that is the correct number')
+    results.push (name + '=' + userScore)
+ }
+}
 
-    let userGuess = ''
-    while (userGuess != number)
-    alert('Guess what the number is!')
+function Scoreboard (){
+    alert(results)
 }
