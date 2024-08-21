5c75623bed4a195126b252167e63a329df609339
Miglia-Schicker <schickmi50@craighead.school.nz>
2024-08-22 10:26:16 +1200

    invaliad testing
    testing numbers outside of the range.
    
    Tested the function GuessTheNumber() with invalid inputs of 28, 0, 32 and got the correct error message output for each one. I also got the output message for a wrong answer which is not needed.

--- script.js
+++ script.js
@@ -12,20 +12,37 @@ function GuessTheNumber() {
 // creating variables and giving them a initial value before they get changed further into the code
     let userScore = 0
     let userGuess = 0
+
+    //computer picks a random number from 1-20 inclusive
     let answer = Math.floor (Math.random()*20)
     answer = answer + 1
+
+
+    //compares users guess to the answer. If it is wrong then the question is asked again and a point is added to their score
     while (userGuess != answer){
         userGuess = prompt ('Guess what the number is!')
         userGuess = userGuess.trim()
-        alert ('That was wrong. Try again')
         userScore = userScore + 1
-  if (userGuess == answer){
-    alert('Well done that is the correct number')
-    results.push (name + ' = ' + userScore)
- } 
+
+        
+        //compares user guess to the answer. If correct, outputs congradgulations message and pushes score onto the scoreboard. If wrong, outputs "Thats wrong. Try again" then the question will repeat.
+        if (userGuess == answer){
+            alert('Well done that is the correct number')
+            results.push (name + ' = ' + userScore)
+         } else if (userGuess != answer){
+            alert ('That was wrong. Try again')
+         }
+        
+
+        if (userGuess > 20 || userGuess < 1){
+            alert('That number seems to be outside of the range. Try again with a number from 1 - 20')
+            userScore = userScore - 1
+         } 
+  
 }
 }
 
+//alerts the scoreboard of people who played its results
 function Scoreboard (){
     alert(results)
 }
