e437b07febc9630c83bc87f27654fe743f6c34a6
Miglia-Schicker <schickmi50@craighead.school.nz>
2024-08-22 14:00:25 +1200

    comments
    added comments to explain how the code works

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// array to store scores
+// array to store users scores
 let results = []
 
 // asks the user for their name and stored and uses the name variable to introduce the game
@@ -6,22 +6,24 @@ function GuessTheNumber() {
   let name = prompt("What is your name?")
   alert("Hi " + name + "! I am thinking of a number between 1 and 20 inclusive. Try and guess the number in the least amount of tries.")
 
-  // creating variables and giving them a initial value before they get changed further into the code
+  // creating variables and giving them a initial value before some get changed further into the code
   let userScore = 0
   let userGuess = 0
   let maximum = 20
   let minimum = 1
   let point = 1
 
-  //computer picks a random number from 1-20 inclusive
+  //computer picks a random number from 1-20 inclusive. Turn the random number into a number in the code instead of string
   let index = Math.floor(Math.random() * 20)
   let answer = index + 1
   answer = Number(answer)
 
-  //compares users guess to the answer. If it is wrong then the question is asked again and a point is added to their score
+  //compares users guess to the answer. If it is wrong then the question is asked again and a point is added to their score.
   while (userGuess != answer) {
     userGuess = prompt("Guess what the number is!")
+    //trims the users guess so that any unnecessary spaces do not affect their guess
     userGuess = userGuess.trim()
+    //Turn the user guess into a number so it can be accurately compared to the answer
     userGuess = Number(userGuess)
     userScore = userScore + point
 
@@ -44,7 +46,7 @@ function GuessTheNumber() {
   }
 }
 
-//alerts the scoreboard of people who played its results
+//outputs the scoreboard of people who played its results
 function Scoreboard() {
   alert(results)
 }
