66a5f82403dcbe948abd9abc3c7608537c0e4b5a
Miglia-Schicker <schickmi50@craighead.school.nz>
2024-08-22 13:39:43 +1200

    invalid testing
    Tested the GuessTheNumber() function with invalid inputs of 0, -50, 32, 57, penguin, banana, one and got the correct error messages for each one

--- script.js
+++ script.js
@@ -12,16 +12,22 @@ function GuessTheNumber() {
 // creating variables and giving them a initial value before they get changed further into the code
     let userScore = 0
     let userGuess = 0
+    let maximum = 20
+    let minimum = 1
+
 
     //computer picks a random number from 1-20 inclusive
-    let answer = Math.floor (Math.random()*20)
-    answer = answer + 1
+    let index = Math.floor (Math.random()*20)
+    let answer = index + 1
+    answer = Number(answer)
+    
 
 
     //compares users guess to the answer. If it is wrong then the question is asked again and a point is added to their score
     while (userGuess != answer){
         userGuess = prompt ('Guess what the number is!')
         userGuess = userGuess.trim()
+        userGuess = Number(userGuess)
         userScore = userScore + 1
 
         
@@ -29,15 +35,21 @@ function GuessTheNumber() {
         if (userGuess == answer){
             alert('Well done that is the correct number')
             results.push (name + ' = ' + userScore)
-         } else if (userGuess != answer){
+         } else if (userGuess != answer && userGuess <= maximum && userGuess >= minimum){
             alert ('That was wrong. Try again')
-         }
-        
-
-        if (userGuess > 20 || userGuess < 1){
+            //Checks if an answer is invalid or outside the number range. Outputs a invalid message then allows the user to try again.
+         } else if (userGuess > maximum || userGuess < minimum){
             alert('That number seems to be outside of the range. Try again with a number from 1 - 20')
             userScore = userScore - 1
+         } else{
+            alert ('That is an invalid answer. Try again with a number from 1 - 20')
+            userScore = userScore - 1
          } 
+
+         
+        
+
+        
   
 }
 }
